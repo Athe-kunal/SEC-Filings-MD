e2c864b738695082841f87d78443889f36d704d6
Athe-kunal <athekunal@gmail.com>
2024-06-04 00:31:17 -0400

    convert single pdf

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ parser.add_argument('-t','--ticker', type=str, help='Ticker of the company')
 parser.add_argument('-y','--year', type=str, help='Year of the filings')
 parser.add_argument('-ia','--include_amends',type=str, help="Whether to include amendment files")
 parser.add_argument('-ft','--filing_types',type=str,help="Filings types separated by commas")
-parser.add_argument("--num_chunks", type=int, default=1, help="Number of chunks being processed in parallel")
-parser.add_argument("--max", type=int, default=None, help="Maximum number of pdfs to convert")
-parser.add_argument("--workers", type=int, default=5, help="Number of worker processes to use")
-
+# parser.add_argument("--num_chunks", type=int, default=1, help="Number of chunks being processed in parallel")
+# parser.add_argument("--max", type=int, default=None, help="Maximum number of pdfs to convert")
+# parser.add_argument("--workers", type=int, default=5, help="Number of worker processes to use")
+parser.add_argument("-bm","--batch_multiplier", type=int, default=2,help="Number of pages to process")
 
 args = parser.parse_args()
 
@@ -28,8 +28,9 @@ run_marker(
     input_ticker_year_path=input_ticker_year_path,
     ticker=args.ticker,
     year=args.year,
-    workers=args.workers,
-    max_workers=args.max,
-    num_chunks=args.num_chunks,
+    # workers=args.workers,
+    # max_workers=args.max,
+    # num_chunks=args.num_chunks,
+    batch_multiplier=args.batch_multiplier,
 )
 print("Done converting")
